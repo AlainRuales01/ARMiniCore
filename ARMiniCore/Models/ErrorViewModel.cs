3a67faa0181f961a6ca47ab66ac066da6ed791fe
AlainRuales01 <alainmateo01@gmail.com>
2022-10-14 17:33:51 -0500

    Add files

---
+++ ARMiniCore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ARMiniCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
