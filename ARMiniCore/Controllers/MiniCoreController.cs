e027975d9a67d622082868f7033e6c8b929055d7
AlainRuales01 <alainmateo01@gmail.com>
2022-10-19 18:06:41 -0500

    Add support for cloud deployment

--- ARMiniCore/Controllers/MiniCoreController.cs
+++ ARMiniCore/Controllers/MiniCoreController.cs
@@ -1,6 +1,7 @@
 ﻿using ARMiniCore.Data.Models;
 using ARMiniCore.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace ARMiniCore.Controllers
 {
@@ -16,10 +17,72 @@ namespace ARMiniCore.Controllers
         public IActionResult Index()
         {
             MiniCoreModel miniCoreModel = new MiniCoreModel();
-            miniCoreModel.Usuarios = _context.Usuario.ToList();
-            miniCoreModel.Pases = _context.Pase.ToList();
-            miniCoreModel.UsuarioPases = _context.UsuarioPase.ToList();
+
+            miniCoreModel = calcularFechas(DateTime.Now.Date);
+             
             return View(miniCoreModel);
+        }
+
+        [HttpPost]
+        public IActionResult Index(DateTime fechaSeleccionada)
+        {
+            
+            MiniCoreModel miniCoreModel = new MiniCoreModel();
+            miniCoreModel = calcularFechas(fechaSeleccionada);
+            return View(miniCoreModel);
+        }
+
+        private MiniCoreModel calcularFechas(DateTime fechaCalcular)
+        {
+            MiniCoreModel miniCoreModel = new MiniCoreModel();
+
+            List<UsuarioPase> usuarioPasesVigentes = new List<UsuarioPase>();
+
+            usuarioPasesVigentes = (from up in _context.UsuarioPase
+                                    join us in _context.Usuario on up.UsuarioId equals us.UsuarioId
+                                    join pa in _context.Pase on up.PaseId equals pa.PaseId
+                                    where DateTime.Compare(fechaCalcular, up.FechaExpiracion) <= 0
+                                    && DateTime.Compare(fechaCalcular, up.FechaCompra) >= 0
+                                    select new UsuarioPase
+                                    {
+                                        FechaExpiracion = up.FechaExpiracion,
+                                        FechaCompra = up.FechaCompra,
+                                        PaseId = up.PaseId,
+                                        UsuarioId = up.UsuarioId,
+                                        Usuario = us,
+                                        Pase = new Pase
+                                        {
+                                            PaseId = pa.PaseId,
+                                            TipoPase = pa.TipoPase,
+                                            Costo = pa.Costo,
+                                            Pases = pa.Pases,
+                                            CostoPase = pa.CostoPase
+                                        }
+                                    }).ToList();
+
+            var usuariosVigentes = (from us in _context.Usuario.ToList()
+                                    join up in usuarioPasesVigentes on us.UsuarioId equals up.UsuarioId
+                                    select us).Distinct().ToList();
+
+            var pases = _context.Pase.ToList();
+
+
+            foreach (var item in usuarioPasesVigentes)
+            {
+                var paseComprado = pases.Where(p => p.PaseId == item.PaseId).FirstOrDefault();
+                if (paseComprado != null)
+                {
+                    var numeroPases = paseComprado.Pases;
+                    var numeroPasesConsumidos = (fechaCalcular - item.FechaCompra).Days;
+                    item.Pase.Pases = numeroPases - numeroPasesConsumidos;
+                }
+            }
+
+            miniCoreModel.Usuarios = usuariosVigentes;
+            miniCoreModel.Pases = pases;
+            miniCoreModel.UsuarioPases = usuarioPasesVigentes;
+
+            return miniCoreModel;
         }
     }
 }
--- ARMiniCore/Models/MiniCoreModel.cs
+++ ARMiniCore/Models/MiniCoreModel.cs
@@ -7,6 +7,7 @@ namespace ARMiniCore.Models
         public List<Usuario> Usuarios { get; set; }
         public List<Pase> Pases { get; set; }
         public List<UsuarioPase> UsuarioPases { get; set; }
+        public DateTime FechaSeleccionada { get; set; }
 
     }
 }
